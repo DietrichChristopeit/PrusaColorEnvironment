2dae483b289a0a1d4103597086fe83b18b9949b6
Dietrich Christopeit <d.christopeit@bluewin.ch>
2022-12-29 05:38:16 +0100

    slowly advancing

--- src/peripherals/sensor/DHTHumTemp.py
+++ src/peripherals/sensor/DHTHumTemp.py
@@ -1,3 +1,8 @@
+"""
+DHTHumTemp.py
+    Sensor for Temp and Humidity - DHT 11 and DHT22
+"""
+
 #   The MIT License (MIT)
 #
 #  Copyright © 2022 <copyright holders>
@@ -9,11 +14,30 @@
 #  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-from micropython import const
+#   The MIT License (MIT)
+#
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#
+#
 
-from peripherals.sensor import DHTHumTemp
+import dht
+from machine import Pin
+from time import sleep
 
-DHT22 = const("22")
-DHT11 = const("11")
+from peripherals.sensor import DHT11
 
-__all__: list[str] = ['DHTHumTemp', 'DHT11', 'DHT22', ]
+
+class DHT:
+    """
+    Temperature and Humidity Sensor
+    """
+
+    def __init__(self, t: str = DHT11, pin: Pin = Pin(13)):
+        self._t = t
+        self._pin = pin
+        self._temperature: float = .0
+        self._humidity: float = .0
+        return
+
